58b201dfb9e1bc41849d67131d65dd32c373ab6f
AdrianRR4 <adrianrr90@hotmail.com>
2021-12-08 23:22:20 -0600

    agregando formulario registro

--- src/app/components/usuario/registro/registro.component.ts
+++ src/app/components/usuario/registro/registro.component.ts
@@ -15,16 +15,26 @@ export class RegistroComponent implements OnInit {
     this.registerForm=this.fb.group({
       user:['',[Validators.required, Validators.email]],
       password:['',[Validators.required,Validators.minLength(6)]],
-      repeatPassword:['  ']
-
-    })
+      repeatPassword:['']
+    },{validator:this.checkPassword})
 
   }
 
   ngOnInit(): void {
   }
   register(){
-    console.log(this.registerForm)
+    console.log(this.registerForm);
+    console.log('hola')
+  }
+
+  checkPassword(group:FormGroup):any{
+
+    const pass=group.controls.password?.value;
+    const confirmPass=group.controls.repeatPassword?.value;
+    console.log(pass);
+    console.log(confirmPass); 
+
+    return pass===confirmPass ? null:{notSame:true}
   }
 
 }
